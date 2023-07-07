1bbaa454571c63eca88ed548e66f56d7fa8655c7
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-07 01:39:02 -0300

    Add files via upload

---
+++ Section3/Section3/books.py
@@ -0,0 +1,8 @@
+import json
+
+with open("books.json", "rt") as file:
+    books = json.load(file)
+
+print(books)
+
+print(type(books))
---
+++ Section3/Section3/iterate_books.py
@@ -0,0 +1,20 @@
+from xml.etree.cElementTree import iterparse
+
+def books(file):
+    for event, elem in iterparse(file):
+        if event == 'start' and elem.tag == 'root':
+            books == elem
+        if event == 'end' and elem.tag == 'book':
+            print('{0}, {1}, {2}, {3}, {4}' .
+                  format(elem.findtext('title'),
+                         elem.findtext('publisher'),
+                         elem.findtext('numberOfChapters'),
+                         elem.findtext('pageCount'),elem.findtext('author')))
+        if event == 'end' and elem.tag == 'chapter':
+            print('{0}, {1}, {2}'.
+                  format(elem.findtext('chapterNumber'),
+                         elem.findtext('chapterTitle'),
+                         elem.findtext('pageCount')))
+
+if __name__ == '__main__':
+    books(open("books.xml"))
